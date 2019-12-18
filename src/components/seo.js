784a3ccf809689873934d7dc543c1c7b39bf78f0
Orest Nowosad <nowosad.orest@gmail.com>
2019-12-18 18:44:28 -0500

    Initial commit

--- src/components/seo.js
+++ src/components/seo.js
@@ -40,18 +40,6 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
         {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
           name: `twitter:card`,
           content: `summary`,
         },
--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,9 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Page Not Found" />
+    <h1>404: NOT FOUND</h1>
+    <p>This page does not exist.</p>
   </Layout>
 )
 
