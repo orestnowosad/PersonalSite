dea7be24bca0469b81ee1b5a478ec7e044d2bebf
Orest Nowosad <nowosad.orest@gmail.com>
2019-12-28 10:14:04 -0500

    Separate webpage scss from base scss

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -4,10 +4,12 @@ import PageLayout from "../components/layouts/pageLayout"
 import Image from "../components/image"
 import SEO from "../components/seo/seo"
 
+import pageStyles from "./page.module.scss";
+
 const ExperiencePage = () => (
   <PageLayout>
     <SEO title="About" />
-    <h1>Experience</h1>
+    <h1 className={pageStyles.pageHeader}>Experience</h1>
     <p>Cool places I've worked at.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,10 +4,12 @@ import PageLayout from "../components/layouts/pageLayout"
 import Image from "../components/image"
 import SEO from "../components/seo/seo"
 
+import pageStyles from "./page.module.scss";
+
 const PortfolioPage = () => (
   <PageLayout>
     <SEO title="About" />
-    <h1>Portfolio</h1>
+    <h1 className={pageStyles.pageHeader}>Portfolio</h1>
     <p>Cool shit that I've built in the past.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
