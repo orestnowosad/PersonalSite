66d30382f880cbd7018e400f61f3469a933470d3
Orest Nowosad <nowosad.orest@gmail.com>
2020-01-06 13:02:03 -0500

    Rename styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,12 +4,12 @@ import PageLayout from "../components/layouts/pageLayout"
 import Image from "../components/image"
 import SEO from "../components/seo/seo"
 
-import pageStyles from "./page.module.scss";
+import styles from "./page.module.scss";
 
 const AboutPage = () => (
   <PageLayout>
     <SEO title="About" />
-    <h1 className={pageStyles.pageHeader}>Who is this guy?</h1>
+    <h1 className={styles.header}>Who is this guy?</h1>
     <p>Just someone that likes to build cool shit.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -4,12 +4,12 @@ import PageLayout from "../components/layouts/pageLayout"
 import Image from "../components/image"
 import SEO from "../components/seo/seo"
 
-import pageStyles from "./page.module.scss";
+import styles from "./page.module.scss";
 
 const ExperiencePage = () => (
   <PageLayout>
     <SEO title="About" />
-    <h1 className={pageStyles.pageHeader}>Experience</h1>
+    <h1 className={styles.header}>Experience</h1>
     <p>Cool places I've worked at.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
