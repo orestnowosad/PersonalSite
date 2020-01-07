3af38bbeb1c405d43cf60cb5fc1a1cd3b8981f69
Orest Nowosad <nowosad.orest@gmail.com>
2020-01-06 19:39:01 -0500

    Implement index page basic structure

--- src/components/layouts/indexLayout.js
+++ src/components/layouts/indexLayout.js
@@ -8,28 +8,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Navbar from "../navbar/navbar"
+import styles from "./indexLayout.module.scss"
 
 const IndexLayout = ({ children }) => {
-  const siteTitle = "Orest Nowosad"
-
   return (
     <>
-      <Navbar siteTitle={siteTitle} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div className={styles.layout}>
+        <main>
+          {children}
+        </main>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import React from "react"
 
 import IndexLayout from "../components/layouts/indexLayout"
-import Image from "../components/image"
 import SEO from "../components/seo/seo"
+
+import styles from "./index.module.scss"
 
 const IndexPage = () => (
   <IndexLayout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className={styles.person}>
+      <h1 className={styles.name}>Orest Nowosad</h1>
+      <h2 className={styles.title}>Software Engineer</h2>
     </div>
   </IndexLayout>
 )
