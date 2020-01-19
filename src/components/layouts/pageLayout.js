195113d21ccba219256a48339b020c05c767c806
Orest Nowosad <nowosad.orest@gmail.com>
2020-01-19 16:10:32 -0500

    Add footer to page layout

--- src/components/layouts/pageLayout.js
+++ src/components/layouts/pageLayout.js
@@ -8,29 +8,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Navbar from "../navbar/navbar"
+import Header from "../header/header"
+import Footer from "../footer/footer"
 
 const PageLayout = ({ children }) => {
   const siteTitle = "Orest Nowosad"
+  const siteDescription = "Software Developer"
 
   return (
     <>
-      <Navbar siteTitle={siteTitle} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <Header siteTitle={siteTitle} />
+      <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
+      <Footer siteTitle={siteTitle} siteDescription={siteDescription} />
     </>
   )
 }
