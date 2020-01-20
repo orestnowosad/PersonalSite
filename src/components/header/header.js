0643897a9df499026960fc47d28ecdcff6c3d14a
Orest Nowosad <nowosad.orest@gmail.com>
2020-01-20 12:12:00 -0500

    Implement header for desktop sizing

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,38 +1,33 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
 import styles from "./header.module.scss"
 
-const Header = ({ siteTitle }) => (
+import logo from "../../assets/images/brand/logo.svg"
+
+const Header = () => (
   <header className={styles.header}>
     <nav className={styles.container}>
-      <h2 className={styles.siteLink}>
-        <Link to="/" className={styles.siteTitle}>
-          {siteTitle}
+      <div className={styles.logo}>
+        <Link to="/">
+          <img src={logo} alt="Brand icon" />
         </Link>
-      </h2>
-      <p className={styles.pageLinks}>
-        <Link to="/about">
-          About
-        </Link>
-        <Link to="/projects">
-          Projects
-        </Link>
-        <Link to="/blog">
-          Blog
-        </Link>
-      </p>
+      </div>
+      <div className={styles.nav}>
+        <p>
+          <Link to="/about">
+            About
+          </Link>
+          <Link to="/projects">
+            Projects
+          </Link>
+          <Link to="/blog">
+            Blog
+          </Link>
+        </p>
+      </div>
     </nav>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
