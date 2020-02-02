285f80c884a84cf410ba2a50c4e5827db4a9af92
Orest Nowosad <nowosad.orest@gmail.com>
2020-02-02 13:14:02 -0500

    Include sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,12 @@ module.exports = {
     title: `Orest Nowosad`,
     description: `Software Developer`,
     author: `@orestnowosad`,
+    siteUrl: `https://orestnowosad.ca`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify-cache`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -26,6 +28,18 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/assets/images/brand-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: `https://orestnowosad.ca`,
+        sitemap: `https://orestnowosad.ca/sitemap.xml`,
+        policy: [{
+          userAgent: `*`,
+          allow: `/`,
+        }],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
